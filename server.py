03b0815da5f11cbf72900fe694a4e545ca2b6df9
JohnyDevp <holan.jan@outlook.com>
2022-07-26 21:36:13 +0200

    next commit

--- server.py
+++ server.py
@@ -2,14 +2,16 @@
 from flask import Flask
 from flask import send_file
 
-#start server flask
-app = Flask(__name__)
 
-#define a route for downloading a pdf file, 
-#TODO there should be a params specification for more sufficient file according to specifications
-@app.route('/download_pdf/')
-def download_pdf():
-    try:
-        return send_file('./out.txt',as_attachment=True)
-    except Exception as e:
-        return str(e)
+if __name__ == "__name__":
+    #start server flask
+    app = Flask(__name__)
+
+    #define a route for downloading a pdf file, 
+    #TODO there should be a params specification for more sufficient file according to specifications
+    @app.route('/download_pdf/')
+    def download_pdf():
+        try:
+            return send_file('./out.txt',as_attachment=True)
+        except Exception as e:
+            return str(e)
